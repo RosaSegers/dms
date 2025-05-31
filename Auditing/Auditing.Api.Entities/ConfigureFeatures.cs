51892e7eb90cd7575c0f599da0b9c546899b5747
Rosa <rosa.segers.2001@gmail.com>
2025-05-31 13:44:24 +0200

    made some changes to the auth

--- Auditing/Auditing.Api.Entities/ConfigureFeatures.cs
+++ Auditing/Auditing.Api.Entities/ConfigureFeatures.cs
@@ -1,8 +1,12 @@
+using System.Text;
+using Auditing.Api.Common.Authorization.Requirements;
 using Auditing.Api.Common.Behaviour;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Auditing.Api.Features
 {
@@ -19,14 +23,25 @@ namespace Auditing.Api.Features
     {
         public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration config)
         {
-            //services.AddSingleton<IAuthorizationHandler, RoleHandler>();
+            services.AddAuthorization(options =>
+            {
+                foreach (var permission in Roles.Items)
+                    options.AddPolicy(permission, policy =>
+                        policy.Requirements.Add(new RoleRequirement(permission)));
+            });
 
-            //services.AddAuthorization(options =>
-            //{
-            //    foreach (var permission in Roles.Items)
-            //        options.AddPolicy(permission, policy =>
-            //            policy.Requirements.Add(new RoleRequirement(permission)));
-            //});
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(config["Jwt:Key"] ?? throw new Exception()))
+                    };
+                });
 
             services.AddMediatR(options =>
             {
