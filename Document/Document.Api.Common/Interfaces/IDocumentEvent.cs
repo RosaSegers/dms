83df2e339866c026d780544be3d4aadae50e29bd
Rosa <rosa.segers.2001@gmail.com>
2025-06-09 10:16:47 +0200

    added the json property

--- Document/Document.Api.Common/Interfaces/IDocumentEvent.cs
+++ Document/Document.Api.Common/Interfaces/IDocumentEvent.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Document.Api.Common.Interfaces
 {
     public interface IDocumentEvent
     {
+        [JsonProperty("id")]
         string Id { get; }
         Guid DocumentId { get; }
         string EventType { get; }
--- Document/Document.Api.Domain/Events/DocumentEventBase.cs
+++ Document/Document.Api.Domain/Events/DocumentEventBase.cs
@@ -1,9 +1,11 @@
 ﻿using Document.Api.Common.Interfaces;
+using Newtonsoft.Json;
 
 namespace Document.Api.Domain.Events
 {
     public abstract class DocumentEventBase : IDocumentEvent
     {
+        [JsonProperty("id")]
         public string Id { get; private set; } = Guid.NewGuid().ToString();
         public Guid DocumentId { get; set; } = Guid.NewGuid();
         public virtual string EventType { get; set; } = string.Empty;
