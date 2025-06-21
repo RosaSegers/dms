1e84f0d839b03823fe08fa250dbf78eec0194e90
Rosa <rosa.segers.2001@gmail.com>
2025-06-21 11:51:28 +0200

    test

--- User/User.Api.Entities/Users/DeleteUser.cs
+++ User/User.Api.Entities/Users/DeleteUser.cs
@@ -51,7 +51,7 @@ namespace User.Api.Features.Users
                 if (user is null)
                     return Error.NotFound("User not found.");
 
-                // This is now using the saga to handle the deletion process
+                // This is now using the saga to handle the deletion process.
                 await userSaga.StartDeleteSagaAsync(request.Id!);
 
                 return Unit.Value;
